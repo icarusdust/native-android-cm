79f2b59e276a980aa88b72653a301f4d67fc1ca4
John <nihalagazade@gmail.com>
2021-06-24 18:06:37 +0300

    inital commit

---
+++ app/src/main/java/com/firstappandroid/androidnativecm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.firstappandroid.androidnativecm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
